c3727d2aadc9a599586bfa30cbf93ff2b4735f4d
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2025-03-03 00:35:13 +0530

    fixes landingpage buges

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -19,8 +19,7 @@ const LandingPage = () => {
       <section className='relative h-screen flex flex-col items-center justify-center text-center bg-linear-to-r from-cyan-500 to-blue-500 text-white px-6'>
         <h1 className='text-5xl font-extrabold leading-tight'>AMS</h1>
         <p className='mt-4 text-lg max-w-2xl'>
-          AI-powered, secure, and efficient attendance management system for
-          your organization.
+        Efficient and hassle-free attendance management.
         </p>
         <button
           onClick={() =>
